1b15a238dd7c11b6bd5d8ab80cc7e1e30c5f6ebb
Michal Manda <mandamic@fit.cvut.cz>
2021-12-03 11:44:07 +0100

    Add video scaling

--- Assets/Scripts/Video/VideoLoader.cs
+++ Assets/Scripts/Video/VideoLoader.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using emt_sdk.Scene;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -6,9 +9,26 @@ public class VideoLoader : MonoBehaviour
     [SerializeField]
     private VideoPlayer _player;
 
+    [SerializeField] 
+    private VideoDisplayComponent _display;
+
+    public void Start()
+    {
+        // TODO: Wait until transform applies!!!!
+        StartCoroutine(DelayApply());
+    }
+
+    private IEnumerator DelayApply()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        _display.Resize(VideoScene.VideoAspectRatioEnum.FitInside);
+    }
+
     public void LoadVideo(string path)
     {
         _player.url = $"file://{path}";
         _player.Play();
+        
+        _display.Resize(VideoScene.VideoAspectRatioEnum.Fill);
     }
 }
