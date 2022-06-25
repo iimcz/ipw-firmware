96a31b56f6f249d3d2ce741b4d0380777de8817a
Michal Manda <mandamic@fit.cvut.cz>
2022-06-25 14:38:41 +0200

    Move NLog initialization to separate component

---
+++ Assets/Scripts/NLogLoaderComponent.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Extensions;
+using NLog.Config;
+using System.IO;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class NLogLoaderComponent : MonoBehaviour
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        [SerializeField]
+        private string _configName = "NLog.config";
+
+        void Start()
+        {
+            var nlogConfig = Path.Combine(Application.streamingAssetsPath, _configName);
+            NLog.LogManager.Configuration = new XmlLoggingConfiguration(nlogConfig);
+
+            Logger.InfoUnity($"Loaded NLog config from '{nlogConfig}'");
+        }
+    }
+}
