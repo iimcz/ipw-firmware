f3a3908882b29e4ae37b2bc0577ddf7d288cb420
Ondřej Slabý <kron258@gmail.com>
2023-04-19 13:48:43 +0200

    Allow specifying a curve for node transition

--- Assets/ScriptsSdk/Navigation/Camera/TransitionRotatorComponent.cs
+++ Assets/ScriptsSdk/Navigation/Camera/TransitionRotatorComponent.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 [RequireComponent(typeof(NodeNavigatorComponent))]
 public class TransitionRotatorComponent : MonoBehaviour
 {
-    public float RotationSpeed = 2f;
+    public AnimationCurve RotationCurve;
 
     private NodeNavigatorComponent _navigator;
+    private NavigationNodeComponent _lastNode;
+    private float _time;
 
     void Start()
     {
@@ -18,10 +20,18 @@ public class TransitionRotatorComponent : MonoBehaviour
         // Stationary, leave it to the other component
         if (_navigator.NextNode == null) return;
 
+        if (_lastNode != _navigator.NextNode)
+        {
+            _time = 0;
+            _lastNode = _navigator.NextNode;
+        }
+
+        _time += Time.deltaTime;
+        var value = RotationCurve.Evaluate(_time);
+
         Vector3 direction = _navigator.NextNode.LookPosition() - transform.position;
         Quaternion toRotation = Quaternion.LookRotation(direction);
-        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, RotationSpeed * Time.deltaTime);
-
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, value);
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
     }
 }
